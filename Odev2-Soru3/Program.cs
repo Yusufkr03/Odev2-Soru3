4940cd90931cae35cb7a6c84440d6bea1391d446
Yusufkr03 <yusufkr2903@gmail.com>
2024-10-31 22:15:47 +0300

    Add files via upload

---
+++ Odev2-Soru3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string nick = "admin";
+const string password = "Admin_32453@";
+string nick1;
+string password1;
+
+Console.WriteLine("Kullanıcı adını giriniz");
+nick1 = Console.ReadLine();
+Console.WriteLine("Şifreyi Giriniz");
+password1 = Console.ReadLine();
+if (nick1 == nick && password1 == password)
+{
+    Console.WriteLine("Giriş başarılı");
+}
+else
+{
+    Console.WriteLine("giriş başarısız");
+}
+Console.ReadKey();
